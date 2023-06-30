53b878c01a582e2e301108139087204ec8e27707
isaip617 <isaip617@icloud.com>
2023-06-29 23:04:53 -0700

    Created Account Data base
    I used sqlite3 to created a database in order to store the information of users . I also included the sql schema to show the constraints and columns that the database has.

---
+++ account_sql_database/account_database.sql
@@ -0,0 +1,73 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE accounts (
+    username TEXT NOT NULL PRIMARY KEY,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE food_spending (
+    username TEXT NOT NULL,
+    date TEXT NOT NULL,
+    food_item TEXT NOT NULL,
+    food_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE clothes_spending (
+    username TEXT NOT NULL,
+    date TEXT NOT NULL,
+    clothes_item TEXT NOT NULL,
+    clothes_item_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE housing_spending (
+    username TEXT NOT NULL,
+    date TEXT NOT NULL,
+    housing_item TEXT NOT NULL,
+    housing_item_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE utilities_spending (
+    username TEXT NOT NULL,
+    date TEXT NOT NULL,
+    utilities_item TEXT NOT NULL,
+    utilities_item_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE transportation_spending (
+    username TEXT NOT NULL,
+    date TEXT NOT NULL,
+    transportation_item TEXT NOT NULL,
+    transportation_item_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE personal_spending (
+    username TEXT NOT NULL,
+    date TEXT NOT NULL,
+    personal_item TEXT NOT NULL,
+    personal_item_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE medical_spending (
+    username TEXT NOT NULL,
+    date TEXT NOT NULL,
+    medical_item TEXT NOT NULL,
+    medical_item_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+
+CREATE TABLE savings_account_categories (
+    username TEXT NOT NULL,
+    retirement_fund DECIMAL(10, 2) NOT NULL,
+    emergency_fund DECIMAL(10, 2) NOT NULL,
+    childrens_college_fund DECIMAL(10, 2) NOT NULL,
+    big_purchase_savings_1 DECIMAL(10, 2) NOT NULL,
+    big_purchase_savings_2 DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (username) REFERENCES accounts(username)
+);
+ 
