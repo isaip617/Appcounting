585a46c226f5c2e0d0ca28c820a40eec280646d6
isaip617 <isaip617@icloud.com>
2023-09-18 17:57:57 -0700

    Created password_checker function
    The password_checker function will grab the password string and first check the password string length, then go through each individual character in the password to see if it has atleast 1 capital letter, 1 lowercase letter, and 1 special character, and if it does it will return true, and if not it will return false.

---
+++ account_creation/database_actions.py
@@ -0,0 +1,25 @@
+def password_checker(password:str) -> bool:
+    """Enter your password and it will check if it fits the qualifications of having atleast
+    1 capital letter, 1 lowercase letter, 1 special character, and is atleast 8 characters long. 
+    If the password meets qualifications the function will return true, if not the function will
+    return false."""
+    list_of_char = [char for char in password]
+    length_of_pw = len(password)
+    capital_letter = False
+    lower_case_letter = False
+    special_character = False
+    password_length = False
+    list_of_special_character = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
+    if length_of_pw >= 8:
+            password_length = True
+    for char in list_of_char:
+        if ord(char) >= 65 and ord(char) <= 90:
+            capital_letter = True
+        if ord(char) >= 97 and ord(char) <= 122:
+            lower_case_letter = True
+        if char in list_of_special_character:
+            special_character = True
+    if capital_letter and special_character and password_length and lower_case_letter:
+        return True
+    else: 
+        return False
