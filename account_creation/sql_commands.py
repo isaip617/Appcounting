e617f0937d991a21bef0afc6437aa242f5baefb4
isaip617 <isaip617@icloud.com>
2023-09-18 18:20:46 -0700

    Created check_for_username function
    The check_for_username function will check the sql data base to see if there is a username that already exists in the database, if so then it will return true, if not it will return false.

--- account_creation/sql_commands.py
+++ account_creation/sql_commands.py
@@ -5,7 +5,24 @@ def insert_username_and_password(username, password:str) -> None:
 
     connection = sqlite3.connect('/Users/isaip617/Desktop/Appcounting/account_sql_database/account_database.db')
     cursor = connection.cursor()
+
     cursor.execute('INSERT INTO accounts (username, password) VALUES (?, ?);', 
     (username, password))
+    
     connection.commit()
     connection.close()
+
+def check_for_username(username:str) -> bool:
+    """Checks how many times the username appears in the sql database, 
+    if it is greater then 0 it will return true, and if is less than or
+    equal to 0 it will return false."""
+
+    connection = sqlite3.connect('/Users/isaip617/Desktop/Appcounting/account_sql_database/account_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT COUNT(*) FROM accounts WHERE username = ?', (username,))
+    count = cursor.fetchone()[0]
+
+    connection.close()
+
+    return count > 0
