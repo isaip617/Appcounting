4930bc73d312e60f254a9d738145da0725117d46
isaip617 <isaip617@icloud.com>
2023-09-19 14:53:52 -0700

    I fixed flash message error
    I was not able to flash a message when a user does not successfully put the correct password or username. It turns out I had to make it so It had to redirect back to the creation page.

--- Account_creation_and_verification/account_creation/account_creation.py
+++ Account_creation_and_verification/account_creation/account_creation.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template, flash, redirect, url_for
 from password_handler import password_checker
 from sql_commands import insert_username_and_password, check_for_username
 
 account_creation = Flask(__name__, template_folder='../templates')
+account_creation.secret_key= "Isai_secret_key"
 
 @account_creation.route('/hello', methods = ["POST", "GET"])
 def login():
@@ -15,11 +16,14 @@ def login():
             insert_username_and_password(username, password)
             return render_template('Homepage.html')
         elif username_validity and not password_validity:
-            flash('Password did not meet qualifications')
+            flash('Password did not meet qualifications', 'info')
+            return redirect(url_for("login"))
         elif not username_validity and password_validity:
-            flash('Username Already in Use')
+            flash('Username Already in Use', 'info')
+            return redirect(url_for("login"))
         elif not username_validity and not password_validity:
-            flash('Username Already in Use and Password did not meet qualifications')
+            flash('Username Already in Use and Password did not meet qualifications', 'info')
+            return redirect(url_for("login"))
     else:
         return render_template("account_creation.html")
 
